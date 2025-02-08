651191b0d6e4629f71cee26055a47d5a9cdb4282
maheshkumarkolla <50858116+maheshkumarkolla@users.noreply.github.com>
2025-02-08 10:46:30 +0000

    added dynamic database

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
 
-from flask import Flask, render_template 
+from flask import Flask, render_template ,jsonify
 app = Flask(__name__ )
+JOBS=[
+
+    {
+    'id':1,
+    'title':"Data analyst",
+    'location':"Bengaluru, India",
+    'salary':"10,00,000"
+
+    },
+
+    {
+    'id':2,
+    'title':'Data scientist',
+    'location':'Delhi, India',
+    'salary':'15,00,000'
+    },
+
+    {
+    'id':3,
+    'title':"Frontend engineer",
+    'location':"San Francisco, USA",
+    
+
+    },
+]
 @app.route("/")
 def hello_world ():
-    return render_template("hello.html")
+    return render_template("hello.html",jobs=JOBS,company_name="Projects")
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 if __name__ == "__main__":
  app.run(host='0.0.0.0', debug=True)
